2b3f4858bff0b1f1ad62df636033681140a78de0
gramizor <granin17@yandex.ru>
2024-01-06 07:42:17 +0300

    da bozhe moi..

--- feedback-frontend-spa/vite.config.ts
+++ feedback-frontend-spa/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/feedback-front",
   plugins: [react()],
   server: {
     proxy: {
